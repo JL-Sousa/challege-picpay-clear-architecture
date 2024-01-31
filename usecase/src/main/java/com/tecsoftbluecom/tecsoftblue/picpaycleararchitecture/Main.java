b2441566db419e88f810b0525694f0fdfc9fea86
Jonyelson Lopes <jonyelsonlopespopos@pop-os.localdomain>
2024-01-31 13:59:15 -0300

    feat: configured the project and its modules

---
+++ usecase/src/main/java/com/tecsoftbluecom/tecsoftblue/picpaycleararchitecture/Main.java
@@ -0,0 +1,7 @@
+package com.tecsoftbluecom.tecsoftblue.picpaycleararchitecture;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
